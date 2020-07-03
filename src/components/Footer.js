eae7bb82292290c7f48a7d70ee1c7e66b4f95f90
Ben Perlmutter <benperlmutter94@gmail.com>
2020-07-03 14:28:28 -0500

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,10 +2,19 @@ import React from 'react';
 import {Footer, Content, Link} from 'arwes';
 import { FaGithub } from 'react-icons/fa'
 const  Foot = () =>{
-
+    const style = { 
+      footer: {
+        padding: '.5rem 2rem 1rem',
+        githubIcon: {
+          position: 'relative',
+          top: 3,
+          paddingLeft: '.25rem'
+        }
+      }
+    }
     return (
       <Footer animate>
-        <Content>
+        <Content style={style.footer}>
           <p>
             Made with great force focus by{" "}
             <Link href="#" target="_blank">
@@ -15,7 +24,14 @@ const  Foot = () =>{
             <Link href="http://ben.perlmutter.io/" target="_blank">
               Ben Perlmutter
             </Link>
-            .{" "} <Link href="" target="_blank"><FaGithub/></Link>
+            .{" "}
+            <Link
+              href="https://github.com/bpmutter/sw-trader-frontend"
+              target="_blank"
+              style={style.footer.githubIcon}
+            >
+              <FaGithub />
+            </Link>
           </p>
         </Content>
       </Footer>
