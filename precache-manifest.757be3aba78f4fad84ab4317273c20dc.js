37c208ab76168a01b2261365cb0e727894b984b7
Ben Perlmutter <benperlmutter94@gmail.com>
2020-07-10 10:56:17 -0500

    Updates

---
+++ precache-manifest.757be3aba78f4fad84ab4317273c20dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3935e98d65b306abfe456701df13d50e",
+    "url": "/startrader/index.html"
+  },
+  {
+    "revision": "5487c1ca2440217851f9",
+    "url": "/startrader/static/css/main.00edf7d3.chunk.css"
+  },
+  {
+    "revision": "4f1b4815ef80e07c2bd8",
+    "url": "/startrader/static/js/2.6bba0a3e.chunk.js"
+  },
+  {
+    "revision": "62f9bbb43a369cf510f80a5278d9f709",
+    "url": "/startrader/static/js/2.6bba0a3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5487c1ca2440217851f9",
+    "url": "/startrader/static/js/main.204d43b9.chunk.js"
+  },
+  {
+    "revision": "700ac6f55a00642bdf34",
+    "url": "/startrader/static/js/runtime-main.b7a999e1.js"
+  },
+  {
+    "revision": "c711a21ca523e54423380239c00cbc20",
+    "url": "/startrader/static/media/starjedi-webfont.c711a21c.woff2"
+  },
+  {
+    "revision": "cceb0e2821ec6b855fe2a1fc136bba44",
+    "url": "/startrader/static/media/starjedi-webfont.cceb0e28.woff"
+  }
+]);
