062c203c2f591c7c13bd60f6ca657ee74312d4ae
Ben Perlmutter <benperlmutter94@gmail.com>
2020-07-10 20:41:27 -0500

    mobile optimize search buttons

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,55 +1,73 @@
 import React, {useState} from 'react';
 import LatestShips from './LatestShips';
 import UniqueShips from './UniqueShips';
-import {Button, Frame} from 'arwes';
+import {Button, Frame, withStyles} from 'arwes';
 import ShipsByType from './ShipsByType';
-const Homepage = () => {
-    debugger;
-    const style = {
-      main: {
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        marginBottom: "3rem",
-      },
-      frame: {
-        margin: ".75rem",
-        maxWidth: 1200,
-      },
-      wrapper: {
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "row",
-        flexWrap: "no-wrap",
-        margin: "3rem",
-        maxWidth: 800,
-        xOverflow: "auto",
-      },
-      button: {
-        margin: ".5rem .5rem",
-      },
-    };
+
+const styles = (theme) => ({
+  main: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    marginBottom: "3rem",
+  },
+  frame: {
+    margin: ".75rem",
+    maxWidth: 1200,
+  },
+
+  buttonWrapper: {
+    // margin: ".5rem",
+    // maxWidth: 800,
+
+    [`@media (max-width: ${theme.responsive.small + 1}px)`]: {
+      boxSizing: 'border-box',
+      display: 'flex',
+      flexWrap: 'nowrap',
+      alignItems: 'center',
+      width: '90vw',
+      padding: '0 .25rem',
+      height: 70,
+      overflowX: 'scroll',
+    },
+  },
+  button: {
+    margin: ".5rem .5rem",
+    [`@media (max-width: ${theme.responsive.small + 1}px)`]: {
+      fontSize: ".75rem",
+    },
+  },
+});
+
+const Homepage = ({classes}) => {
 
     const [activeQuery, setActiveQuery] = useState('latest');
     
     return (
-      <main style={style.main}>
-        <div style={style.wrapper}>
-          <Button style={style.button} onClick={()=>{setActiveQuery("latest")}}>Latest</Button>
-          <Button style={style.button} onClick={()=>{setActiveQuery("unique")}}>Unique</Button>
-          <Button style={style.button} onClick={()=>{setActiveQuery("Starfighter")}}>Starfighters</Button>
-          <Button style={style.button} onClick={()=>{setActiveQuery("Star Dreadnought")}}>Dreadnoughts</Button>
-          <Button style={style.button} onClick={()=>{setActiveQuery("Star Cruiser")}}>Cruisers</Button>
-          <Button style={style.button} onClick={()=>{setActiveQuery("Freighter")}}>Freighters</Button>
-          <Button style={style.button} onClick={()=>{setActiveQuery("Transport")}}>Transports</Button>
-          
+      <main className={classes.main}>
+          <Frame
+            animate
+          level={3}
+          corners={2}
+          layer="primary"
+          className={classes.frame}
+          >
+        <div className={classes.buttonWrapper}>
+            <Button className={classes.button} onClick={()=>{setActiveQuery("latest")}}>Latest</Button>
+          <Button className={classes.button} onClick={()=>{setActiveQuery("unique")}}>Unique</Button>
+          <Button className={classes.button} onClick={()=>{setActiveQuery("Starfighter")}}>Starfighters</Button>
+          <Button className={classes.button} onClick={()=>{setActiveQuery("Star Dreadnought")}}>Dreadnoughts</Button>
+          <Button className={classes.button} onClick={()=>{setActiveQuery("Star Cruiser")}}>Cruisers</Button>
+          <Button className={classes.button} onClick={()=>{setActiveQuery("Freighter")}}>Freighters</Button>
+          <Button className={classes.button} onClick={()=>{setActiveQuery("Transport")}}>Transports</Button>
         </div>
+          </Frame>        
         <Frame
           animate
           level={3}
           corners={4}
           layer="primary"
-          style={style.frame}
+          className={classes.frame}
         >
           {activeQuery === "latest" && <LatestShips/>}
           {activeQuery === "unique" && <UniqueShips/>}
@@ -60,4 +78,4 @@ const Homepage = () => {
     );
 }
 
-export default Homepage;
+export default withStyles(styles)(Homepage);
